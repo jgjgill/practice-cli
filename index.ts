16bf99581bdd08e816f9ec239f96552ae595818a
jgjgill <dbdltm22@naver.com>
2024-04-09 19:05:48 +0900

    add more stuff

--- index.ts
+++ index.ts
@@ -1,5 +1,42 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {};
+import { Command } from "commander";
 
-console.log("wowow");
+const program = new Command();
+
+program
+  .argument("<string>", "string to log")
+  .option("-c, --capitalize", "Capitalize the message")
+  .action(
+    (
+      message: string,
+      opts: {
+        capitalize?: boolean;
+      }
+    ) => {
+      if (opts.capitalize) {
+        console.log(message.toUpperCase());
+      } else {
+        console.log(message);
+      }
+    }
+  )
+  .description("Say hello");
+
+program
+  .command("add <numbers...>")
+  .action((numbers: number[]) => {
+    const total = numbers.reduce((a, b) => Number(a) + Number(b), 0);
+    console.log(`Total ${total}`);
+  })
+  .description("Add numebers and log the total");
+
+program
+  .command("get-max-number <numbers...>")
+  .action((numbers: number[]) => {
+    const max = Math.max(...numbers);
+    console.log(`Max: ${max}`);
+  })
+  .description("Add numbers and log the total");
+
+program.parse(process.argv);
